0c30d0e9bee37862ddbed731c1c5caf5cef6afd2
Acquah <emanuel.molina2k@gmail.com>
2023-03-30 00:52:04 -0700

    add arrays for all criterias needed

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
-// Assignment code here
+var allCriterias = ["Do you want lowercase letters?", "Do you want uppercase letters?"
+,"do you want numbers?", "Do you want special characters?"];
 
+var getPassword = document.querySelector("#generate");
+
+// Special Characters
+var character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+// Numeric characters
+var number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// Lowercase characters
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+// Uppercase characters
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+
+// object.onclick = function criteriaList() {
+//   var getLengthPassword = "How long do you want your password to be?";
+//   var lengthNotSet = "you need to set a length.";
+//   if (prompt(getLengthPassword) == true) {
+//      for (){
+//       all
+//      }
+//   }else{
+//     alert(lengthNotSet);
+//   }
+// }
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
