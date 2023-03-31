ba9e4e1fb7e9b6a3222ba8faa4507c7fa4d58030
Acquah <emanuel.molina2k@gmail.com>
2023-03-30 23:18:24 -0700

    add user inputs for all charcters combonations

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+var finalPassword = [];
+
+var chosenCriterias;
 
 var getPassword = document.querySelector("#generate");
 
 // Special Characters
-var character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+var specialCharacters = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
 // Numeric characters
 var number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 // Lowercase characters
@@ -28,20 +31,55 @@ function generatePassword(){
       var userConfirmLC = confirm("Will your password have lowercase letters?");
       var userConfirmUC = confirm("Will your password have uppercase letters?");
     }
-}
 
+    // if no criterias are picked
+    if (!userConfirmSC && !userConfirmNum && !userConfirmLC && !userConfirmUC){
+        alert("You have to choose atleast one criteria!!");
 
-// object.onclick = function criteriaList() {
-//   var getLengthPassword = "How long do you want your password to be?";
-//   var lengthNotSet = "you need to set a length.";
-//   if (prompt(getLengthPassword) == true) {
-//      for (){
-//       all
-//      }
-//   }else{
-//     alert(lengthNotSet);
-//   }
+      // if all criterias are picked
+      }else if (userConfirmSC && userConfirmNum && userConfirmLC && userConfirmUC){
+        chosenCriterias = specialCharacters.concat(number, lowerCase, upperCase);
+        // all picked criterias mixed with special character 
+    }else if (userConfirmSC && userConfirmNum && userConfirmLC){
+        chosenCriterias = specialCharacters.concat(number, lowerCase);
+    }else if (userConfirmSC && userConfirmNum && userConfirmUC){
+      chosenCriterias = specialCharacters.concat(number, upperCase);
+    }else if (userConfirmSC && userConfirmUC && userConfirmLC){
+        chosenCriterias = specialCharacters.concat(lowerCase, upperCase);
+    }else if (userConfirmSC && userConfirmNum){
+        chosenCriterias = specialCharacters.concat(number);
+    }else if (userConfirmSC && userConfirmLC){
+        chosenCriterias = specialCharacters.concat(lowerCase);
+    }else if (userConfirmSC && userConfirmUC){
+        chosenCriterias = specialCharacters.concat(upperCase);
+    }else if (userConfirmSC){
+        chosenCriterias = specialCharacters;
+    }
+      // all picked criterias mixed with numbers
+    else if (userConfirmNum && userConfirmLC && userConfirmUC){
+        chosenCriterias = number.concat(lowerCase, upperCase);
+    }else if (userConfirmNum && userConfirmLC){
+        chosenCriterias = number.concat(lowerCase);
+    }else if (userConfirmNum){
+        chosenCriterias = number;
+    }
+      // all picked criteria mixed with lower case
+    else if (userConfirmLC && userConfirmUC){
+        chosenCriterias = lowerCase.concat(upperCase);
+    }else if (userConfirmLC){
+        chosenCriterias = lowerCase;
+    }
+      // all picked criterias mixed with upper case
+    else if (userConfirmUC){
+        chosenCriterias = upperCase
+    }
+
+//       //for loop that randomizes a password for the user
+//     for (var i = 0; i < userInput; i++){
+//       var selectedCriteria = chosenCriterias[Math.floor]
+//     }
 // }
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
